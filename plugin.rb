5eb6c6aa895bb3edd135ce708a7171647e5a567a
Alexandre Marcondes <alex@marcondesian.net>
2018-03-17 22:52:47 -0400

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: Debt Syndicate Discourse Theme
+# about: Plugin to bend Discourse to the Debt Syndicate's aesthetic will
+# version: 0.0.1
+# authors: Debt Syndicate Developers
+
+register_asset 'stylesheets/common/debtsyndicate.scss'
