26bb5df830d92453a9eb34bd6afed29c48be48c7
arath <me@arathjz.com>
2020-07-18 14:49:00 -0500

    feat(discourse-theme): script to update discourse theme

---
+++ scripts/update-theme.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+function updateTheme(themeId, baseUrl, apiKey, apiUsername) {
+  fetch(`${baseUrl}/admin/themes/${themeId}`, {
+    headers: {
+      "content-type": "application/json",
+      "Api-Key": `${apiKey}`,
+      "Api-Username": `${apiUsername}`
+    },
+    body: JSON.stringify({
+      theme: {
+        remote_update: true
+      }
+    }),
+    method: "PUT"
+  })
+    .then(response => response.json())
+    .then(() => console.log("Theme updated successfully 🚀"))
+    .catch(error => console.error(error));
+}
+
+updateTheme(
+  process.env.DISCOURSE_THEME_ID,
+  process.env.DISCOURSE_APP_BASE_URL,
+  process.env.DISCOURSE_API_KEY,
+  process.env.DISCOURSE_API_USERNAME
+);
