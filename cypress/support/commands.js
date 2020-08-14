2e34830ad403c624212a37d160436a8a5014d7d3
Alexis Duran <alexis.duranm@gmail.com>
2020-08-14 18:30:35 +0200

    test(login): complete login flow test
    this commit will add custom commands to help the replication and verification of the flow. Mainly,
    we request for the new user to the API and activate through the API too, then we are able to login
    without pass through the whole flow of filling forms which ensure isolate test for the login only
    and speed up the test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,42 @@
-import "@testing-library/cypress/add-commands";
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import { buildUser } from "../support/generate";
+
+const authHeaders = {
+  "Api-Key": Cypress.env("DISCOURSE_API_KEY"),
+  "Api-Username": Cypress.env("DISCOURSE_USERNAME"),
+  "Content-Type": "application/json"
+};
+
+Cypress.Commands.add("createUser", (overrides, userId) => {
+  const user = buildUser(overrides);
+
+  cy.request({
+    url: `/users`,
+    method: "POST",
+    body: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      username: user.username,
+      // zipCode and phoneNumber are required custom fields
+      user_fields: {
+        "2": user.zipCode,
+        "3": user.phoneNumber
+      }
+    },
+    headers: authHeaders
+  }).then(response => {
+    cy.request({
+      url: `/admin/users/${response.body.user_id}/activate`,
+      method: "PUT",
+      headers: authHeaders
+    }).then(({ success }) => ({ ...user }));
+  });
+});
+
+Cypress.Commands.add("assertHome", () => {
+  cy.url().should("eq", `${Cypress.config().baseUrl}/`);
+});
+
+Cypress.Commands.add("assertLoggedInAs", user => {
+  cy.get(`img[title="${user.name}"]`).should("be.visible");
+});
