3ac24fada937324e1c30af2fec1f304429b0f4d3
Orlando Del Aguila <orlando@hashlabs.com>
2021-02-11 14:20:08 -0600

    feat(email-login): redirect user to after_signup_path when login via email for the first time
    We will redirect users to the member hub.

---
+++ javascripts/discourse/initializers/dc-email-login.js
@@ -0,0 +1,42 @@
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "dc-email-login",
+  initialize() {
+    withPluginApi("0.8.24", (api) => {
+      api.modifyClass("controller:emailLogin", {
+        actions: {
+          finishLogin() {
+            let data = {
+              second_factor_method: this.secondFactorMethod,
+              timezone: moment.tz.guess(),
+            };
+            if (this.securityKeyCredential) {
+              data.second_factor_token = this.securityKeyCredential;
+            } else {
+              data.second_factor_token = this.secondFactorToken;
+            }
+
+            ajax({
+              url: `/session/email-login/${this.model.token}`,
+              type: "POST",
+              data: data,
+            })
+              .then((result) => {
+                if (result.success) {
+                  const redirectTo = result.redirect_to || "/";
+
+                  DiscourseURL.redirectTo(redirectTo);
+                } else {
+                  this.set("model.error", result.error);
+                }
+              })
+              .catch(popupAjaxError);
+          },
+        },
+      });
+    });
+  },
+};
